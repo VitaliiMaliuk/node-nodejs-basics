7f12291246bd0f2cc531a766422460d55a8c4f10
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2023-06-16 17:08:48 +0300

    feat: rename function finished

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,34 @@
+import { rename as _rename, access } from "fs/promises";
+import { join } from "path";
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    const sourceFile = join("src/fs/files", "wrongFilename.txt");
+    const destinationFile = join("src/fs/files", "properFilename.md");
+
+    const sourceExists = await checkFileExists(sourceFile);
+    if (!sourceExists) {
+      throw new Error("FS operation failed: Source file does not exist");
+    }
+
+    const destinationExists = await checkFileExists(destinationFile);
+    if (destinationExists) {
+      throw new Error("FS operation failed: Destination file already exists");
+    }
+
+    await _rename(sourceFile, destinationFile);
+    console.log("File renamed successfully.");
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
+const checkFileExists = async (filePath) => {
+  try {
+    await access(filePath);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
 await rename();
