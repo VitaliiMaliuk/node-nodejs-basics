597b64efb1feb22f9698a93a81c6e48282d14583
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2023-06-16 15:57:43 +0300

    feat: create function finish

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,29 @@
+import fs, { access, writeFile } from "fs/promises";
+import { join } from "path";
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const filePath = join("src/fs", "files", "fresh.txt");
+        let fileExists = await checkFileExists(filePath);
+        if (fileExists) {
+            throw new Error("FS operation failed: File already exists");
+        } else {
+            await writeFile(filePath, "I am fresh and young");
+            console.log("File created successfully.");
+            fileExists = true;
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
+const checkFileExists = async (filePath) => {
+    try {
+        await access(filePath);
+        return true;
+    } catch (error) {
+        return false;
+    }
 };
 
 await create();
