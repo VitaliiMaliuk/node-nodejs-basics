1df5d148e8ba7de5e0d0b10a3a4690ecb2d70aee
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2023-06-17 12:09:38 +0300

    feat: list function finished

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,30 @@
+import { readdir, stat } from "fs/promises";
+import { join } from "path";
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const folderPath = join("src/fs", "files");
+
+    const folderExists = await checkDirectoryExists(folderPath);
+    if (!folderExists) {
+      throw new Error("FS operation failed: Folder does not exist");
+    }
+
+    const files = await readdir(folderPath);
+    files.forEach((file) => {
+      console.log(file);
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
+const checkDirectoryExists = async (directoryPath) => {
+  try {
+    const stats = await stat(directoryPath);
+    return stats.isDirectory();
+  } catch (error) {
+    return false;
+  }
+};
 await list();
