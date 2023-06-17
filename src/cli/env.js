40b89f1e37f23eb8139c341417f3c287fc447ec8
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2023-06-17 12:55:39 +0300

    feat: parseEnv function finished

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = "RSS_";
+    const variables = [];
+    for (const key in process.env) {
+        if (key.startsWith(prefix)) {
+            const name = key.substring(prefix.length);
+            const value = process.env[key];
+            variables.push(`RSS_${name}=${value}`);
+        }
+    }
+    const output = variables.join("; ");
+    console.log(output);
 };
 
 parseEnv();
