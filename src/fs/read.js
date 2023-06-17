c4cfbdcb64a118662a5a427d91d87cbff7a0f636
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2023-06-17 12:36:32 +0300

    feat: read file function finished

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,28 @@
+import { readFile, access } from "fs/promises";
+import { join } from "path";
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const filePath = join("src/fs/files", "fileToRead.txt");
+
+        const fileExists = await checkFileExists(filePath);
+        if (!fileExists) {
+            throw new Error("FS operation failed: File does not exist");
+        }
+
+        const content = await readFile(filePath, "utf-8");
+        console.log(content);
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
+const checkFileExists = async (filePath) => {
+    try {
+        await access(filePath);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
 await read();
