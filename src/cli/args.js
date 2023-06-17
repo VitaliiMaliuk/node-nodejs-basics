fb3bf6f523c18fcdb7369b369f20e1a07f5f0617
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2023-06-17 13:04:52 +0300

    feat: parseArgs function finished

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const parsedArguments = {};
+
+  for (let i = 0; i < args.length; i += 2) {
+    const propertyName = args[i].substring(2);
+    const propertyValue = args[i + 1];
+    parsedArguments[propertyName] = propertyValue;
+  }
+
+  for (const key in parsedArguments) {
+    const value = parsedArguments[key];
+    console.log(`${key} is ${value}`);
+  }
 };
 
 parseArgs();
