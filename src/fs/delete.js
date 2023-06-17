f487fb04daf42a3668dc7468ec5a080b0f9b9713
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2023-06-17 11:55:49 +0300

    feat: delete function finished

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,28 @@
+import { unlink, access } from "fs/promises";
+import { join } from "path";
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        const filePath = join("src/fs/files", "fileToRemove.txt");
+
+        const fileExists = await checkFileExists(filePath);
+        if (!fileExists) {
+            throw new Error("FS operation failed: File does not exist");
+        }
+
+        await unlink(filePath);
+        console.log("File deleted successfully.");
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
+const checkFileExists = async (filePath) => {
+    try {
+        await access(filePath);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
 await remove();
