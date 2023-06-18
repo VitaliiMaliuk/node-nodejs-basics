a6f343716c118969371505abe0d8376cae73f18e
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2023-06-18 12:27:22 +0300

    feat: readstream function finished

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream } from "fs";
+import { join } from "path";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join("src/streams/files", "fileToRead.txt");
+  const readableStream = createReadStream(filePath);
+  readableStream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
+  readableStream.on("end", () => {
+    process.stdout.write("\n");
+  });
+  readableStream.on("error", (error) => {
+    throw new Error("FS operation failed: " + error.message);
+  });
 };
 
 await read();
