f1bee40c6e5df42d20317efa269b70f4b4d257d0
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2023-06-18 13:46:00 +0300

    feat: decompress function finished

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import { createReadStream, createWriteStream } from "fs";
+import { join } from "path";
+import { createGunzip } from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const compressedFile = join("src/zip/files", "archive.gz");
+  const decompressedFile = join("src/zip/files", "fileToCompress.txt");
+
+  const readStream = createReadStream(compressedFile);
+  const writeStream = createWriteStream(decompressedFile);
+  const gunzipStream = createGunzip();
+
+  await new Promise((resolve, reject) => {
+    readStream
+      .pipe(gunzipStream)
+      .pipe(writeStream)
+      .on("finish", resolve)
+      .on("error", reject);
+  });
+
+  console.log(
+    `File ${compressedFile} decompressed to ${decompressedFile}`
+  );
 };
 
 await decompress();
