e2075d4ec6baec482a2e910fd876d5853f84fad6
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2023-06-18 13:09:06 +0300

    feat: write and transform functions finished

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { createWriteStream } from "fs";
+import { join } from "path";
+
 const write = async () => {
-    // Write your code here 
+  const filePath = join("src/streams/files", "fileToWrite.txt");
+  const writableStream = createWriteStream(filePath);
+  process.stdout.pipe(writableStream);
+  console.log(`Writing data from process.stdin to ${filePath}`);
+  writableStream.on("finish", () => {
+    console.log(`Data has been written to ${filePath}`);
+  });
 };
 
 await write();
