c4ea8ea8dc7d7dbab906fa5d6bf224f2c0705577
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2023-06-18 11:49:27 +0300

    feat: hash function finished

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { promises } from "fs";
+import { createHash } from "crypto";
+import { join } from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const filePath = join("src/hash/files", "fileToCalculateHashFor.txt");
+
+    const fileContent = await promises.readFile(filePath);
+
+    const hash = createHash("sha256");
+    hash.update(fileContent);
+    const hexHash = hash.digest("hex");
+    console.log(`SHA256 Hash: ${hexHash}`);
+  } catch (error) {
+    throw new Error("FS operation failed: " + error.message);
+  }
 };
 
-await calculateHash();
+await calculateHash().catch((error) => console.error(error));
